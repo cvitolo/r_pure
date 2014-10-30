eb9c6ff9112bb5b3e72d032ce9d7c24ea0cc833e
Claudia Vitolo <claudia.vitolo@gmail.com>
2014-10-30 13:38:05 +0000

    Added two new functions: - EventIndentification - calculateCN

--- pure/R/EventIdentification.R
+++ pure/R/EventIdentification.R
@@ -1,66 +1,105 @@
 #' Identify Rainfall-Runoff events
 #'
 #' @param data data.frame of precipitation (P) and discharge (Q) time series
-#' @param dt data time step
 #'
 #' @author Claudia Vitolo
 #'
-#' @return areal averaged time series
+#' @return data.frame containing 3 columns: Tr (return period), P (max precipitation) and Q (max discharge)
 #'
 
-EventIdentification <- function(data,dt){
+EventIdentification <- function(data){
 
   # require(zoo)
   # require(EcoHydRology)
+  # data <- DATA[1:500,c("P","Q")]
 
-  # STEP 1
+  ### STEP 1 ###
   # Separate baseflow from runoff
   bfs <- zoo(BaseflowSeparation(coredata(data$Q))[,"bt"], order.by=index(data))
   ## You can check out how this looks with the hydrograph function:
   # hydrograph(input=data.frame("datetime"=index(data),"P"=coredata(data$P),
   #                             "Q"=coredata(data$Q),"Qbase"=coredata(bfs[,1])))
-  # Calculate the median of baseflow to use as threshold
-  threshold <- median(bfs)
+
   # Use quick flow only (runoff - baseflow)
   qfl <- zoo(BaseflowSeparation(coredata(data$Q))[,"qft"], order.by=index(data))
 
-  # STEP 2
-  # Identify discrete RAINFALL events from time series using hydromad::eventseq.
-  evp <- eventseq(data$P,        # precipitation time series
-                  thresh = 0.1,  # threshold value
-                  inthresh = 0,  # second threshold to define when events stop
-                  indur = 6)     # P must remain below inthresh for indur time
-                                 # steps in order to terminate an event
-  ## You can check out the structure of the evp object
-  # str(evp)
+  # set up a warmup period
+  percentageWarmUp <- 10
+  warmup <- round(dim(data)[1]/percentageWarmUp,0)
+  pperiod <- (warmup+1):dim(data)[1]
 
-  # STEP 3
-  # Calculate rainfall centroid
-  numberOfEvents <- dim(eventinfo(data$P,evp))[1]
-  for (n in 1:numberOfEvents) {
-    start <- eventinfo(data$P,evp)$Time[n]
-    end   <- eventinfo(data$P,evp)$Time[n] + eventinfo(data$P,evp)$Duration[n]*x
-    sumP  <- /sum(coredata(window(data$P,start=start,end=end)))
-  }
+  bfs <- bfs[pperiod]
+  qfl <- qfl[pperiod]
+  dataNew <- window(data, start=index(data)[warmup+1],end=index(data)[dim(data)[1]])
+  dataNew$Q <- qfl
+  data <- data.frame("datetime"=index(data)[pperiod],"P"=data[pperiod,"P"],"Q"=qfl)
 
-  # Extend the events to include RUNOFF recession limb.
-  events <- data.frame("Start"     = eventinfo(data$P,evp)$Time,
-                       "End"       = rep(NA,numberOfEvents),
-                       "CentroidP" = rep(NA,numberOfEvents),
-                       "CentroidQ" = rep(NA,numberOfEvents),
-                       "")
-
-  # STEP 3
-  # Calculate centroids of rainfall and runoff
-  for (event in 1:dim(eventinfo(data$P,evp))[1]){
-
-    eventinfo(data$P,evp)$Time
-
-  }
   ## You can check out how this looks with the hydrograph function:
-  # hydrograph(input=data.frame("datetime"=index(data), "P"=coredata(data$P),
-  #                             "Q"=coredata(data$Q), "Qbase"= bfs[,1]))
+  # hydrograph(input=data.frame("datetime"=index(dataNew),"P"=coredata(dataNew$P),
+  #                             "Q"=coredata(dataNew$Q)))
 
+  ### STEP 2 ###
+  # Identify discrete RR events from time series using hydromad::eventseq.
+  # This function returns a zoo object, with core data consisting of an ordered
+  # factor, representing the identified events, and the same time index as x.
+  # Periods between events are left as NA, unless all = TRUE in which case they
+  # are treated as separate events. The returned object stores thresh as an
+  # attribute. Typical input parameters are:
+  # x        = P and Q(quick) time series
+  # thresh   = threshold value
+  # inthresh = second threshold to define when events stop
+  # mindur   = the minimum number of time steps in each event window
+  # indur    = P must remain below inthresh for indur time steps in order to terminate an event
+  # mingap   = the the minimum number of time steps that can separate events
+  evpq <- eventseq(x       = dataNew[,c("P","Q")],
+                  thresh   = c(0.1, median(qfl)),
+                  inthresh = c(0,   median(qfl)),
+                  mindur   = 6,
+                  indur    = 1,
+                  mingap   = 2)
+  ## You can check out the structure of the evp object
+  # str(evpq)
+  #
+  ## Plot
+  xyplot(dataNew) + layer_(panel.xblocks(evpq, col = c("grey90", "grey80"), border = "grey80"))
 
+  ### STEP 3 ###
+  # Order events in descending order and calculate matching return periods
+  eventsP <- sort(eventinfo(dataNew$P, evpq, FUN = max)$Value, decreasing = TRUE)
+  returnPeriodP <- (length(eventsP)-1)/(1:length(eventsP))
+  dP <- data.frame(x=returnPeriodP, y=eventsP)
+  # loglogplot(dP)
+
+  eventsQ <- sort(eventinfo(dataNew$Q, evpq, FUN = max)$Value, decreasing = TRUE)
+  returnPeriodQ <- (length(eventsQ)-1)/(1:length(eventsQ))
+  dQ <- data.frame(x=returnPeriodQ, y=eventsQ)
+  # loglogplot(dQ)
+
+  ### STEP 4 ###
+  # Calculate the regression function to model dP and dQ
+
+  # Calculate the regression function that describes dP
+  # modelP.lm <- lm(formula = y ~ x + I(x^2), data = dP)
+  modelP.lm <- lm(formula = y ~ log(x), data = dP)
+  # Calculate the regression function that describes dQ
+  # modelQ.lm <- lm(formula = y ~ x + I(x^2), data = dQ)
+  modelQ.lm <- lm(formula = y ~ log(x), data = dQ)
+  # Define some return periods
+  Tr <- 2:round(max(returnPeriodP,returnPeriodQ),0)
+  # Use predict to estimate the values for the return period.
+  # Note that predict expects a data.frame and the col names need to match
+  newP <- predict(modelP.lm, newdata = data.frame(x = Tr))
+  newQ <- predict(modelQ.lm, newdata = data.frame(x = Tr))
+
+  # plot(dP$x, dP$y, type="o", ylim=c(min(dP$y,newP),max(dP$y,newP)))
+  # points(Tr, newP, col = "red")
+
+  # plot(dQ$x, dQ$y, type="o", ylim=c(min(dQ$y,newQ),max(dQ$y,newQ)))
+  # points(Tr, newQ, col = "red")
+
+  # Final data.frame
+  df <- data.frame("Tr"=Tr,"P"=newP,"Q"=newQ)
+
+  return(df)
 
 }
--- pure/R/calculateCN.R
+++ pure/R/calculateCN.R
@@ -1,34 +1,21 @@
+#' Calculate the Curve Number from P and Q
+#'
+#' @param P maximum precipitation for an event (it can also be a vector, for multiple events)
+#' @param Q maximum discharge for an event (it can also be a vector, for multiple events)
+#'
+#' @author Claudia Vitolo
+#'
+#' @return Curve Number, in the range [0,100]
+#'
+
 calculateCN <- function(P,Q){
 
   if (length(P)!=length(Q)) stop
 
-  myCN <- rep(NA,length(P))
+  S <- 5*P + 10*Q - sqrt( (5*P+10*Q)^2 -25*(P^2-P*Q) )
 
-  for (r in 1:length(P)){
+  CN <- 1000/(S + 10)
 
-    # f <- function (CN) ((0.371-0.2*(1000/CN - 10))^2) / (0.371+0.8*(1000/CN - 10)) - 0.03
-
-    f <- function (CN) ((P[r]-0.2*(1000/CN - 10))^2) / (P[r]+0.8*(1000/CN - 10)) - Q[r]
-
-    # uniroot(f, c(0, 100), myP = P[r], myQ = Q[r], tol = 0.1)
-    # temp <- try(uniroot(f, c(0, 100), myP = P[r], myQ = Q[r], tol = 0.1)$root, TRUE)
-    # curve(f, 0, 100)
-    x <- c()
-
-    for (c in 1:101) x[c] <- f(c-1)
-
-    minRange <- min(which(!is.na(x) & x>0)-1)
-
-    maxRange <- max(which(!is.na(x) & x<0)-1)
-
-    print(paste("CN for event n.",r,"is calculated in the range from",minRange, "to", maxRange))
-
-    temp <- round(uniroot(f, c(minRange, maxRange), tol = 0.10)$root,0)
-
-    if (is.numeric(temp)) myCN[r] <- temp
-
-  }
-
-  return(myCN)
+  return(CN)
 
 }
